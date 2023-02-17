0aea723954feedcc25622ebcafb1a1c16f0b0cc4
Jules-infotech <julius.smooth.jj@gmail.com>
2023-02-17 07:58:41 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- This is an entry
+ * Return: must  Always end with the 'zero' return
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
