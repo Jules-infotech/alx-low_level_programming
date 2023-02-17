61e6f5a42fe8ab45738e832e927e3f03839e42ca
Jules-infotech <julius.smooth.jj@gmail.com>
2023-02-17 08:16:15 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is an entry value
+ * return: always return 'zero' as the return value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
