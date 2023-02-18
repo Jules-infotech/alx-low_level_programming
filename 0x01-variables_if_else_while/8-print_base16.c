c8304351477efb6580d66d87c9fea409baad1026
Jules-infotech <julius.smooth.jj@gmail.com>
2023-02-17 16:25:49 -0800

    Hexadecimal print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all number in Hexadecimal
+ * Return: Always 'zero'
+ */
+int main(void)
+{
+	char c = 'a';
+	int d = 0;
+
+	while (d < 10)
+	{
+		putchar(d + '10');
+		d++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
