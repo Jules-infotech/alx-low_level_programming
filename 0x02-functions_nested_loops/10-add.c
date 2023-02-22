04b07be1efebab277c3455c845b3776e4c13d143
Jules-infotech <julius.smooth.jj@gmail.com>
2023-02-22 01:37:44 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -  a function that adds two integers and returns the result
+ *
+ * Return: result
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+
+	result = a + b;
+	return (result);
+}
