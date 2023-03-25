c3167e8d81af57d66880a46b882a1afb74bd5439
Jules-infotech <julius.smooth.jj@gmail.com>
2023-03-25 16:55:15 -0700

    Update

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is a readme file on 0x10-variadic functions task
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+_putchar;
+int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
